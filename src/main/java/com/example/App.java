e638012921ff56e015269bcd7dd52f6780ac8cb0
Sakshi <sakshitina01@gmail.com>
2024-09-05 00:31:35 +0530

    Your commit message

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class MyWebApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public String greet() {
+        return "Hello, World!";
+    }
+}
